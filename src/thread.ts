ba00c1e6f63b24efacca55e148e06baf275472ba
Philipp Legner <philipp@mathigon.org>
2019-11-16 03:52:36 +0000

    Update WebWorkers / threading

--- src/thread.ts
+++ src/thread.ts
@@ -7,56 +7,46 @@
 import {defer} from '@mathigon/core';
 
 
-declare global {
-  interface Window {
-    Worker?: Worker;
-  }
+/**
+ * Converts a function into a WebWorker URL object that can be passed into
+ * thread(). Note that `fn` has to be a single function with no external
+ * references or bindings, so that it can be stringified using .toString().
+ */
+export function functionToWorker(fn: Function) {
+  const content = `onmessage = e => postMessage((${fn.toString()})(e.data))`;
+  const blob = new Blob([content], {type: 'application/javascript'});
+  return URL.createObjectURL(blob);
 }
 
 
 /**
- * Executes a function in a separate thread, for improved performance. Note that
- * `fn` has to be a single function with no external references or bindings, so
- * that it can be stringified using .toString(). Similarly, `args` has to be a
- * single number or string, or an array or numbers and strings
+ * Creates a new web worker, posts it a serializable data object, and returns
+ * when the worker responds (or after a fixed timeout).
  */
-export function thread<T>(fn: (...args: number[]) => T,
-                          args: number|number[],
-                          timeout = 5000): Promise<{data: T, time: number}> {
+export function thread<T = any>(url: string|URL, data: any, timeout = 5000) {
+  const deferred = defer<T>();
+  const worker = new Worker(url);
 
-  if (!Array.isArray(args)) args = [args];
-
-  if (!window.Worker || !window.Blob) {
-    return Promise.resolve({data: fn(...args), time: 0});
-  }
-
-  const deferred = defer<{data: T, time: number}>();
-  const start = Date.now();
-
-  const content = 'onmessage = function(e){return postMessage(eval(e.data[0]));}';
-  const blob = new Blob([content], {type: 'application/javascript'});
-  const w = new Worker(URL.createObjectURL(blob));
-
-  const t = setTimeout(function () {
-    w.terminate();
-    deferred.reject('Timeout!');
+  const t = setTimeout(() => {
+    worker.terminate();
+    console.error('WebWorker timeout!');
+    deferred.reject();
   }, timeout);
 
-  w.onmessage = (e: MessageEvent) => {
+  worker.onmessage = (e: MessageEvent) => {
     clearTimeout(t);
-    w.terminate();
-    deferred.resolve({data: e.data, time: Date.now() - start});
+    worker.terminate();
+    console.log(e);
+    deferred.resolve(e.data);
   };
 
-  w.onerror = (e: ErrorEvent) => {
+  worker.onerror = (e: ErrorEvent) => {
     clearTimeout(t);
-    console.error('WebWorker error', e);
-    w.terminate();
+    console.error('WebWorker error!', e);
+    worker.terminate();
     deferred.reject(e);
   };
 
-  // TODO Find a solution that works without function stringification.
-  w.postMessage(['(' + fn.toString() + ')(' + args.join(',') + ')']);
-
+  worker.postMessage(data);
   return deferred.promise;
 }
