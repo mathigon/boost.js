82c552f42fec975e860f5418122b598cd34622c2
Philipp Legner <philipp@mathigon.org>
2021-01-16 22:36:21 +0100

    Improved SVG path parsing

--- src/svg.ts
+++ src/svg.ts
@@ -4,33 +4,66 @@
 // =============================================================================
 
 
-import {last} from '@mathigon/core';
+import {chunk, last} from '@mathigon/core';
 import {Point} from '@mathigon/euclid';
 
 
-const ITEM_SIZE: Record<string, number> = {C: 6, S: 4, Q: 4, A: 7};
-const SEGMENTS = /[MmLlCcSsQqTtAa][0-9,.\-\s]+/g;
-const NUMBERS = /-?([0-9]*\.)?[0-9]+/g;
+export type PathCommand = {points: Point[], type?: string, options?: number[]};
 
-export function parsePath(d: string) {
-  if (!d) return [];
+const pathLength: Record<string, number> = {A: 7, C: 6, H: 1, L: 2, M: 2, Q: 4, S: 4, T: 2, V: 1, Z: 0};
+const pathSegment = /[astvzqmhlc]([^astvzqmhlc]*)/ig;
+const pathPoint = /-?[0-9]*\.?[0-9]+(?:e[-+]?\d+)?/ig;
 
-  const segments = d.match(SEGMENTS) || [];
-  const points: Point[] = [];
 
-  for (const s of segments) {
-    // Space before - sign is not required!
-    const items = (s.slice(1).match(NUMBERS) || []).map(x => +x);
-    const type = s[0].toUpperCase();
-    const isRelative = (type !== s[0]);
-    const itemLength = ITEM_SIZE[type] || 2;
+export function pathCommands(path: string): PathCommand[] {
+  const commands: PathCommand[] = [];
+  let lastPoint: Point|undefined = undefined;
 
-    for (let i = 0; i < items.length; i += itemLength) {
-      const x = items[i + itemLength - 2];
-      const y = items[i + itemLength - 1];
-      points.push(isRelative ? last(points).shift(x, y) : new Point(x, y));
+  for (const match of (path.match(pathSegment) || [])) {
+    const uType = match[0].toUpperCase();
+
+    if (uType === 'Z') {
+      commands.push({type: 'Z', points: []});
+      continue;
+    }
+
+    const args = (match.slice(1).match(pathPoint) || []).map((p: string) => +p);
+    const isAbsolute = uType === match[0];
+
+    for (const [i, p] of chunk<number>(args, pathLength[uType]).entries()) {
+      let points: Point[] = [];
+      let type = (uType === 'M' && i > 0) ? 'L' : uType;
+      let options: number[]|undefined = undefined;
+
+      if (uType === 'H') {
+        type = 'L';
+        points = [new Point(p[0], isAbsolute ? (lastPoint?.y || 0) : 0)];
+      } else if (uType === 'V') {
+        type = 'L';
+        points = [new Point(isAbsolute ? (lastPoint?.x || 0) : 0, p[0])];
+      } else if (uType === 'A') {
+        type = 'A';
+        points = [new Point(p[5], p[6])];
+        options = p.slice(0, 5);
+      } else if ('MLCSQT'.includes(uType)) {
+        points = chunk(p, 2).map(q => new Point(q[0], q[1]));
+      }
+
+      if (!isAbsolute && lastPoint) points = points.map(p => p.translate(lastPoint!));
+      lastPoint = last(points);
+      commands.push({type, points, options});
     }
   }
 
-  return points;
+  return commands;
+}
+
+/**
+ * Return all points on an SVG path. Essentially, this turns a curved path into
+ * a polygon with just the joins/corners selected.
+ */
+export function parsePath(d: string): Point[] {
+  if (!d) return [];
+  const commands = pathCommands(d);
+  return commands.map(c => last(c.points)).filter(p => !!p);
 }
