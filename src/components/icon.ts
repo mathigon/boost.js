b69ae90db09828898b1c418394ec528a60eaf7fd
Philipp Legner <philipp@mathigon.org>
2022-02-04 15:27:43 +0100

    Global variable to update icon strings

--- src/components/icon.ts
+++ src/components/icon.ts
@@ -6,6 +6,9 @@
 
 import {$N, CustomElementView, register} from '../';
 
+// Added by the build progress to inline custom icons.
+declare const ICONS: Record<string, string>|undefined;
+const IconPaths = ICONS;
 
 /**
  * SVG icon component, which loads from a global `/icons.svg` file.
@@ -24,8 +27,12 @@ export class IconView extends CustomElementView {
     const $svg = $N('svg', {viewBox: '0 0 24 24'}, this);
     $svg.css({width: `${size}px`, height: `${size}px`});
 
-    const $use = $N('use', {}, $svg);
-    this.onAttr('name', (n) => $use.setAttr('href', `/icons.svg#${n}`));
+    if (IconPaths) {
+      this.onAttr('name', (n) => ($svg.html = IconPaths[n]));
+    } else {
+      const $use = $N('use', {}, $svg);
+      this.onAttr('name', (n) => $use.setAttr('href', `/icons.svg#${n}`));
+    }
 
     // TODO ARIA attributes / alt text
     // TODO Maybe polyfill if <use> is not supported
